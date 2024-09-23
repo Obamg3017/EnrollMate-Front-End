10c13adde8232dce122c1d0e5a736783823771e1
FemiBam <Femibam1@gmail.com>
2024-09-22 20:42:49 -0400

    completed functionality on nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import "./navbar.css";
+import { FaSearch } from "react-icons/fa"; // Make sure to install react-icons if you haven't
 
-export const Navbar = ({ student, handleSignOut }) => {
+export const Navbar = ({ student, handleSignOut,setQuery }) => {
+  const [input, setInput] = useState("");
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleClick = () => {
+    setQuery(input);
+    navigate("/courses");
+  };
+
+  console.log(pathname);
+  const getNavClassName = () => {
+    if (pathname === "/") {
+      return "visitor-nav"; // Home - transparent background
+    } else if (pathname === "/students/dashboard") {
+      return ""; // Dashboard - white background
+    }
+    return "";
+  };
   return (
-    <nav>
-      <div>
-        <h1>EM</h1>
-      </div>
-      <div>
-        {student ? (
-          <>
-            <Link to="/students/dashboard">Student Dashboard</Link>
-            <span onClick={handleSignOut}>Sign Out</span>
-          </>
-        ) : (
+    <nav className={getNavClassName()}>
+      {pathname === "/" && (
+        <div className="nav-right signin-button">
           <Link to="/users/login">Sign In</Link>
+        </div>
+      )}
+
+      {pathname === "/students/dashboard" && (
+        <div className="dashboard-nav">
+          <img src="/EM-SD-logo.png" alt="logo" className="logo" />
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="searchBar"
+              onChange={handleChange}
+            />
+            <FaSearch onClick={handleClick} className="search-icon" />
+          </div>
+          <div className="right-nav-buttons">
+            <Link to="/students/dashboard">Student Dashboard</Link>
+            <Link onClick={handleSignOut}>Sign Out</Link>
+          </div>
+        </div>
+      )}
+      {pathname !== "/students/dashboard" &&
+        pathname !== "/" &&
+        pathname !== "/users/login" &&
+        pathname !== "/students/register" && (
+          <div className="dashboard-nav">
+            <img src="/EM.png" alt="logo" className="logo" />
+
+            <div></div>
+
+            {student ? (
+              <div className="right-nav-buttons">
+                <Link to="/students/dashboard">Student Dashboard</Link>
+                <Link onClick={handleSignOut}>Sign Out</Link>
+              </div>
+            ) : (
+              <div className="nav-right signin-button">
+                <Link to="/users/login">Sign In</Link>
+              </div>
+            )}
+          </div>
         )}
-      </div>
     </nav>
   );
 };
-
